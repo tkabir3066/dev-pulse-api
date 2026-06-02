a917fb2ebf91343cc38de875c08ee93693bb3063
Tutul <tutulkabir@gmail.com>
2026-06-03 02:10:46 +0530

    modified in auth routes

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -1,14 +1,9 @@
 import { Router } from "express";
 import { AuthController } from "./auth.controller";
-import auth from "../../middlewares/auth";
 
 const router = Router();
 
-router.post(
-  "/signup",
-  //   auth("contributor", "maintainer"),
-  AuthController.registerUser,
-);
+router.post("/signup", AuthController.registerUser);
 router.post("/login", AuthController.login);
 
 export const AuthRoutes = router;
